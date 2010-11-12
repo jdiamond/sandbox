7f07b14eea3e3e78b8a4938454557f6e281084c3
Jason Diamond <jason@diamond.name>
2010-11-12 09:27:18 -0800

    Added NaturalSorting project.

---
+++ dotnet/NaturalSorting/NaturalSortingTests.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace NaturalSorting
+{
+    [TestFixture]
+    public class NaturalSortingTests
+    {
+        [Test]
+        public void NaturalSortingCanBeDoneByPaddingAllTheNumbers()
+        {
+            var unsorted = new[]
+                         {
+                             "10.10.10",
+                             "9.9.9"
+                         };
+
+            var sorted = unsorted.OrderBy(s => s.PadNumbers(2));
+
+            CollectionAssert.AreEqual(new[]
+                                      {
+                                          "9.9.9",
+                                          "10.10.10"
+                                      },
+                                      sorted.ToArray());
+        }
+    }
+}
---
+++ dotnet/NaturalSorting/PadNumbersExtension.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace NaturalSorting
+{
+    public static class PadNumbersExtension
+    {
+        private static readonly Regex NumbersRegex = new Regex(@"\d+");
+
+        public static string PadNumbers(this string value, int totalWidth)
+        {
+            return NumbersRegex.Replace(value, m => m.Value.PadLeft(totalWidth, '0'));
+        }
+    }
+}
